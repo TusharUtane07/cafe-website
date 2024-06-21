60690d5a52400943006c5e3c0122d95d48012a3b
Tushar Utane <tusharutane2@gmail.com>
2024-06-21 08:24:28 +0530

    Gallery page done

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,46 @@
-const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+import gallery1 from "../assets/gallery1.jpg";
+import gallery2 from "../assets/gallery2.jpg";
+import gallery3 from "../assets/gallery3.jpg";
+import gallery4 from "../assets/gallery4.jpg";
+import gallery5 from "../assets/gallery5.jpg";
+import gallery6 from "../assets/gallery6.jpg";
+import gallery7 from "../assets/gallery7.jpg";
+import gallery8 from "../assets/gallery1.jpg";
 
-export default Gallery
+const Gallery = () => {
+	return (
+		<div className="flex items-center justify-center">
+			<div className="grid m-5 gap-y-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-3">
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery1} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery2} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery3} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery4} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery5} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery6} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery7} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery8} alt="" />
+				</div>
+				<div>
+					<img className="w-96 h-80 rounded-md" src={gallery3} alt="" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Gallery;
